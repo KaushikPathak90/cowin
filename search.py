e48e94265fb829e6b28332ef776c3cfbe54ced29
Kaushik Pathak <45171300+KaushikPathak90@users.noreply.github.com>
2021-06-02 15:39:12 +0530

    Update search.py

--- search.py
+++ search.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 import time
 
 options = Options()
@@ -16,24 +17,30 @@ def get_date_time():
 class Cowin:
     def __init__(self):
         # Download the latest version of Chromedriver from
-        # https://chromedriver.storage.googleapis.com/89.0.4389.23/chromedriver_win32.zip
+        # https://chromedriver.storage.googleapis.com/91.0.4472.19/chromedriver_win32.zip
         # and unzip it and put the path here -
         self.driver = webdriver.Chrome(r"C:\Users\kaush\Downloads\chromedriver_win32_91\chromedriver.exe", options=options)
-        # Open chrome/firefox browser
+        # Open chrome browser
 
         self.driver.get("https://www.cowin.gov.in/home")
         # Launch the URL
+        self.driver.maximize_window()
+        self.driver.save_screenshot("./image" + get_date_time() + ".png")
 
         time.sleep(2)
         self.driver.find_element(By.XPATH, "/html/body/app-root/div/app-home/div[2]/div/appointment-table/div/div/div/div/div/div/div/div/div/div/form/mat-tab-group/mat-tab-header/div[2]/div/div/div[3]/div").click()
         self.driver.find_element(By.XPATH, "/html/body/app-root/div/app-home/div[2]/div/appointment-table/div/div/div/div/div/div/div/div/div/div/form/mat-tab-group/div/mat-tab-body[3]/div/div/div[1]/mat-form-field/div/div[1]/div/mat-select/div/div[2]").click()
         self.driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div/div/div/mat-option[37]/span").click()                                                                                                                         
         #West Bengal
+        time.sleep(1)
+        self.driver.save_screenshot("./image" + get_date_time() + ".png")
 
         time.sleep(2)
         self.driver.find_element(By.XPATH, "/html/body/app-root/div/app-home/div[2]/div/appointment-table/div/div/div/div/div/div/div/div/div/div/form/mat-tab-group/div/mat-tab-body[3]/div/div/div[2]/mat-form-field/div/div[1]/div/mat-select/div/div[2]/div").click()
         self.driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div/div/div/mat-option[29]/span").click()
         #West Bardhaman
+        time.sleep(1)
+        self.driver.save_screenshot("./image" + get_date_time() + ".png")
 
     def search(self):            
 
@@ -43,23 +50,9 @@ class Cowin:
         self.driver.find_element(By.XPATH, "/html/body/app-root/div/app-home/div[2]/div/appointment-table/div/div/div/div/div/div/div/div/div/div/div[2]/form/div/div/div[2]/div[1]/div/div[1]/label").click() 
         #Age 18+
 
-        ## Current World Population
-        #print("Current World Population: " + self.driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div[2]/div/div[1]/div/span").text.title()) 
 
-        ## Today:  Births, Deaths and population growth today
-        #print("TODAY")
-        #print("Births today: " + self.driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div[2]/div/div[3]/div[1]/div/div[2]/div[2]/span").text.title())
-        #print("Deaths today: " + self.driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div[2]/div/div[3]/div[1]/div/div[3]/div[2]/span").text.title())
-        #print("Population Growth today: " + self.driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div[2]/div/div[3]/div[1]/div/div[4]/div[2]/span").text.title())
-
-        ## This Year:  Births, Deaths and population growth today
-        #print("THIS YEAR")
-        #print("Births this year: " + self.driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div[2]/div/div[3]/div[2]/div/div[2]/div[2]/span").text.title())
-        #print("Deaths this year: " + self.driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div[2]/div/div[3]/div[2]/div/div[3]/div[2]/span").text.title())
-        #print("Population Growth this year: " + self.driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div[2]/div/div[3]/div[2]/div/div[4]/div[2]/span").text.title())
-
-
-n = 100000
+#n = 100000
+n = 5
 bot = Cowin()
 try:
     i = 1
@@ -67,6 +60,9 @@ try:
         print("-----\nLoop: " + str(i))
         bot.search()
         time.sleep(5)
+        if i == 1:
+            for _ in range(10):
+                bot.driver.find_element(By.XPATH,"/html").send_keys(Keys.ARROW_DOWN)
         bot.driver.save_screenshot("./image" + get_date_time() + ".png")
         i += 1
         if i > n:
